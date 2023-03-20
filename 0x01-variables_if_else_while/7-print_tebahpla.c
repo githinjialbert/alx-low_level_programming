4c5036fed0a502562f956b09c6e79679058c54c6
githinjialbert <githinjialbert01@gmail.com>
2023-03-20 06:56:27 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+*main - Entry point
+*Description: Smile in the mirror
+*Return: always 0
+*/
+int main(void)
+{
+	int w = 122;
+
+	while (w >= 97)
+	{
+		putchar(w);
+		w++;
+	}
+	putchar('\n');
+return (0);
+}
