a26d6c511fe842bb47e56fecf463975b2aceddeb
githinjialbert <githinjialbert01@gmail.com>
2023-03-20 07:40:12 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+*main - Entry point
+*Description: persistence makes an unbeatable combination for success
+*Return: always 0
+*/
+int main(void)
+{
+	int y;
+
+	for (y = 48; y <= 57; y++)
+	{
+		putchar(y);
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
