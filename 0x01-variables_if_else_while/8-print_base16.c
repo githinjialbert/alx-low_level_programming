18d2eb87a4354b983f7d1a7ca49253fe612e5ada
githinjialbert <githinjialbert01@gmail.com>
2023-03-20 07:18:40 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+*main - Entry point
+*Description:Hexadecimal
+*Return: always 0
+*/
+int main(void)
+{
+	int w;
+	int r;
+
+	for (w = 48; w <= 48; w++)
+	{
+		putchar(w);
+		w++;
+	}
+	for (r = 97; r <= 102; r++)
+	{
+		putchar(r);
+		r++;
+	}
+	putchar('\n');
+	return (0);
+}
