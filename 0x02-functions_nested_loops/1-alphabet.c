e503a2e977da36a18aa7624eca42f54cc806154c
githinjialbert <githinjialbert01@gmail.com>
2023-03-21 02:37:05 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+*main - Entry point
+*Description: I sometimes suffer from insomnia
+*Return: always 0
+*/
+void print_alphabet(void)
+{
+	for (n = 97; n <= 122 n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
