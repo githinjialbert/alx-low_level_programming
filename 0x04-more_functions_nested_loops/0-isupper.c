9559e82d7200efb18b7fe6e3a1b38052e9fb3011
githinjialbert <githinjialbert01@gmail.com>
2023-03-23 05:37:12 -0700

    0-isupper.c'
    clear
    aklfjaksljdfklsdjfa

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,11 +1,12 @@
 #include "main.h"
 /**
 *isupper - checks for uppercase character.
+*Return:1 if c is a digit 0 otherwise
 */
 int _isupper(int c)
 {
-	int start = 65, end = 90;  
-		if (c >= start && c <= end)
-			return (1);
-		return (0);
+	int start = 65, end = 90;
+	if (c >= start && c <= end)
+		return (1);
+	return (0);
 }
