b2721e29e4c3f7a87e4b6d02c5f8b68dc5d19479
githinjialbert <githinjialbert01@gmail.com>
2023-03-24 01:21:37 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+/**
+*print_numbers -  prints 0-9
+*Return: void 
+*/
+void print_numbers(void)
+{
+	int k;
+
+	for (k = 48; k <= 57; k++)
+	{
+		putchar(k);
+	}
+	putchar('\n');
+}
