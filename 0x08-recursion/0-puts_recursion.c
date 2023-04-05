1e17c373ab57549994185843911a9bdbcace4a3c
githinjialbert <githinjialbert01@gmail.com>
2023-04-05 00:47:59 -0700

    0-puts_recursion.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+/**
+*_puts_recursion -  prints a string
+*@s: string
+*Return: always 0
+*/
+void _puts_recursion(char *s)
+{
+        if (*s == '\0')
+        {
+                putchar('\n');
+                return;
+        }
+        putchar(*s);
+        _puts_recursion(s + 1);
+}
