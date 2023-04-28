50d3d432af2b4bffbcd6c2b2fbc5fc0e1af957d4
githinjialbert <githinjialbert01@gmail.com>
2023-04-28 13:21:21 -0700

    100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+*print_intro - prints the string
+*Retrun: always 0
+*/
+void print_intro(void)
+{
+	printf("Your'e beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
