8efac0ababf3d368906acacbef0f7679ddd33180
githinjialbert <githinjialbert01@gmail.com>
2023-05-05 02:37:13 -0700

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -15,7 +15,7 @@ unsigned int binary_to_uint(const char *b)
 		return (0);
 	for (v = 0; b[v] != '\0'; v++)
 	{
-		if (*b != '0' && *b != '1')
+		if (b[v] != '0' && b[v] != '1')
 		{
 			return (0);
 		}
