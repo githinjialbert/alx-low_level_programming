c055dd20ba19c4ef6c7aa049dec4bd2276ea87fa
githinjialbert <githinjialbert01@gmail.com>
2023-05-12 03:14:33 -0700

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -13,35 +13,20 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	char *buffer;
 
 	if (filename == NULL)
-	{
 		return (0);
-	}
-	buffer = malloc(sizeof(char) * letters);
+
+	buffer = malloc(letters);
 	if (buffer == NULL)
 		return (0);
 
 	o = open(filename, O_RDONLY);
-	if (o == -1)
-	{
-		free(buffer);
-		return (0);
-	}
 	r = read(o, buffer, letters);
-	if (r == -1)
-	{
-		free(buffer);
-		close(o);
-		return (0);
-	}
-	w = write(buffer, r, STDOUT_FILENO);
-	if (w == -1 || (size_t)w != r)
-	{
-		free(buffer);
-		close(o);
-		return (0);
-	}
-	free(buffer);
-	close(o);
+	w = write(STDOUT_FILENO, buffer, r);
 
-	return(w);
+	if (o == -1 || r == -1 || w == -1 || w != r)
+	{
+		free(buffer);
+		close(o);
+	}
+	return (w);
 }
--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,24 +11,9 @@
 int create_file(const char *filename, char *text_content)
 {
 	int fd;
-	int num_written = 0;
+	ssize_t o, r, w;
 
-	if (filename == NULL)
-		return (-1);
+	if (filename == NULL )
+		return (0);
 
-	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
-	if (text_content != NULL)
-	{
-		int len = strlen(text_content);
-
-		num_written = write(fd, text_content, len);
-		if (num_written < 0)
-		{
-			close(fd);
-			return (-1);
-		}
-	}
-	close(fd);
-	return (-1);
-}
