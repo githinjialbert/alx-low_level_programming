28fa95f88c60108f6efa694366e2be0af2087d4f
githinjialbert <githinjialbert01@gmail.com>
2023-05-12 11:24:58 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,8 +16,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (text_content != NULL)
 	{
-		for (len = 0; text_content[len];)
-		len++;
+		for (len = 0; text_content[len]; len++);
 	}
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC | 0600);
 	w = write(fd, text_content, len);
@@ -25,8 +24,9 @@ int create_file(const char *filename, char *text_content)
 	{
 		return (-1);
 	}
-	if (w == -1)
+	if (w == -1 || w != len)
 	{
+		close(fd);
 		return (-1);
 	}
 	close(fd);
