24a267580a2656a8e9da6bf0017b45743dcb6d5f
githinjialbert <githinjialbert001@gmail.com>
2023-06-08 08:15:44 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -6,6 +6,46 @@
 */
 void print_times_table(int n)
 {
-	if (n > 15 || n < 0)
+	int u, v, w;
+
+	if (n <= 0 || n >= 15)
 		return;
 
+	for (u = 0; u <= n; u++)
+	{
+		for (v = 0; v <= n; v++)
+		{
+			w = u * v;
+
+			if (v == 0)
+			{
+				putchar(w + '0');
+			}
+			else if (w < 10 && v != 0)
+			{
+				putchar(',');
+				putchar(' ');
+				putchar(' ');
+				putchar(' ');
+				putchar(w + '0');
+			}
+			else if (w < 100 && w >= 10)
+			{
+				putchar(',');
+				putchar(' ');
+				putchar(' ');
+				putchar((w / 10) + '0');
+				putchar((w % 10) + '0');
+			}
+			else if (w >= 100)
+			{
+				putchar(',');
+				putchar(' ');
+				putchar((w / 100) + '0');
+				putchar(((w / 10) % 10) + '0');
+				putchar((w % 10) + '0');
+			}
+		}
+		putchar('\n');
+	}
+}
