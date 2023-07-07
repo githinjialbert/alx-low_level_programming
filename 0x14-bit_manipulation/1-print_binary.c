2667e2ea73d9f843e96f5a36db6d89b209897962
githinjialbert <githinjialbert001@gmail.com>
2023-07-07 03:51:15 -0700

    1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -7,10 +7,11 @@
 */
 void print_binary(unsigned long int n)
 {
-	if (n > 1)
+	if (n >> 0)
 	{
+		if (n >> 1)
 		print_binary(n >> 1);
-	}
-	else
 		putchar((n & 1) + '0');
+	}
+	else putchar ('0');
 }
