624f9283fb625bf3310e10af991bf62308e3e12b
githinjialbert <githinjialbert001@gmail.com>
2023-07-11 03:07:43 -0700

    1-create_file.c

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+*create_file - creates a file
+*@filename: the name of the file
+*@text_content: the string to write to the file
+*Return: 1 on success -1 on failure
+*/
+int create_file(const char *filename, char *text_content)
+{
+	int file;
+	int Byteswritten;
+
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+	file = open(filename, O_CREAT | O_TRUNC | O_WRONLY | 0600);
+	if (file == -1)
+	{
+		return (-1);
+	}
+	if (text_content != NULL)
+	{
+		Byteswritten = write(file, text_content, strlen(text_content));
+	}
+	if (Byteswritten == -1)
+	{
+		close(file);
+		return(-1);
+	}
+	close(file);
+	return (1);
+}
