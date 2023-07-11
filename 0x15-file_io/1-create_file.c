e6b2578d21ab9fd74a0580e6aa9d8c061a651a9c
githinjialbert <githinjialbert001@gmail.com>
2023-07-11 03:12:48 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,7 +14,7 @@ int create_file(const char *filename, char *text_content)
 	{
 		return (-1);
 	}
-	file = open(filename, O_CREAT | O_TRUNC | O_WRONLY | 0600);
+	file = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
 	if (file == -1)
 	{
 		return (-1);
@@ -26,7 +26,7 @@ int create_file(const char *filename, char *text_content)
 	if (Byteswritten == -1)
 	{
 		close(file);
-		return(-1);
+		return (-1);
 	}
 	close(file);
 	return (1);
