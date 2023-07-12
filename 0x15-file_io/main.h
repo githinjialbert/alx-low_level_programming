944fdbe67e8d4178c1b63d86f5f9836f90ae7ae9
githinjialbert <githinjialbert001@gmail.com>
2023-07-12 14:14:02 -0700

    3-cp.c

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -22,6 +22,9 @@ int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+char *createBuffer(char *name);
+void closeFile(int file);
+int main(int argc, char *argv[]);
 
 
 
